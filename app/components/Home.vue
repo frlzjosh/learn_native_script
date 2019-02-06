40071f73b5cd2d4d58fd64a31ca89c61eaa2b732
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-02-05 17:31:32 -0800

    LOOK AT MY FIRST MY COMMIT

---
+++ app/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+    <Page class="page">
+        <ActionBar class="action-bar">
+            <Label class="action-bar-title" text="Home"></Label>
+        </ActionBar>
+
+        <GridLayout>
+            <Label class="title__bar" :text="title" row="1" col="1"></Label>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                title: "Josh's Native App",
+                sup: 'lol',
+            }
+        },
+        computed: {
+            message() {
+                return "Blank {N}-Vue app";
+            }
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    // Start custom common variables
+    @import '../app-variables';
+    // End custom common variables
+
+    // Custom styles
+    .fa {
+        color: $accent-dark;
+    }
+
+    .info {
+        font-size: 20;
+    }
+</style>
